a01ee7827bb259aa52057ce64851963406a06dc9
aminjafer <amin.jafer@hotmail.com>
2022-11-17 22:02:03 +0500

    Get request is working in the Server App

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ import bodyParser from "body-parser";
 import mongoose, { mongo } from "mongoose";
 import cors from 'cors';
 import dotenv from "dotenv";
+import postRoutes from './routes/posts.js'
 
 const app = express();
 
 dotenv.config();
 
 //what to use 
+app.use('/posts', postRoutes);
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended:true}));
 app.use(cors()); 
