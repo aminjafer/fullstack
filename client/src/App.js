ed1fdaae3591bc6971bc7e84ca9af15130021298
aminjafer <amin.jafer@hotmail.com>
2022-11-17 18:57:00 +0500

    MongoDb connected and first default route defined for server app

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,9 @@ import React from 'react';
 const App = () => {
     return (
         <div>
-            <h1>App</h1>
+            <h1>This is an App</h1>
         </div>
     );
 }
+
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import cors from 'cors';
+import dotenv from "dotenv";
 
+const app = express();
+
+dotenv.config();
+
+//what to use 
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended:true}));
+app.use(cors()); 
+
+//monogodb host on the cloud atlas
+const CONNECTION_URL = process.env.MONGODB_URI_WITH_CREDS;
+const PORT = process.env.PORT || 5063;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server runnign on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);
