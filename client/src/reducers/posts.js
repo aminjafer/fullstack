4692947e190e73ad7b5d45d816673da8e6b605d9
aminjafer <amin.jafer@hotmail.com>
2022-11-20 00:53:07 +0500

    Like button is working perfectly!

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,6 +1,10 @@
 export default (posts = [], action) => {
     switch(action.type) {
         case 'UPDATE': 
+            console.log('Update reducer fired!');
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post );
+            
+        case 'LIKE': 
             return posts.map((post) => post._id === action.payload._id ? action.payload : post );
 
         case 'FETCH_ALL': 
