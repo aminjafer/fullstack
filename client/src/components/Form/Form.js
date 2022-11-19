5e603466ea97a050e24c3d7ce35f05365efbfb16
aminjafer <amin.jafer@hotmail.com>
2022-11-19 15:25:44 +0500

    Posts are showing and background color also set

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -34,7 +34,7 @@ const Form = () => {
             <div className={classes.fileInput}>
                 <FileBase type="file" multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64})}/> 
             </div>
-            <Button className={classes.buttonSubmit} variant="container" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+            <Button className={classes.buttonSubmit} variant="contained" color="Primary" size="large" type="submit" fullWidth>Submit</Button>
             <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
             </form>
         </Paper>
